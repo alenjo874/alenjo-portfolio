a8a92a31a6f4ba8953ea966da4034c6ece39741f
Alen Jo <alenjo874@gmail.com>
2022-02-15 13:55:29 -0500

    nothing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from "./components/Pages/Home/Home"
-
+import logo from "./logo.svg";
+import "./App.css";
+import PagesContainer from "./components/Pages/PagesContainer";
+import Home from "./components/Pages/Home/Home";
+import NavBarConatiner from "./components/Header/NavBarContainer"
 
 function App() {
   return (
     <div className="App">
-     <Home />
+      <Home />
+      <NavBarConatiner />
+      <PagesContainer />
     </div>
   );
 }
