8836ae34d507f32c9441d946e28d1095dbab6339
Alen Jo <alenjo874@gmail.com>
2022-03-08 20:55:46 -0500

    changed nav bar style

--- src/components/Header/NavBarContainer.js
+++ src/components/Header/NavBarContainer.js
@@ -8,12 +8,16 @@ function NavBarContainer() {
   return (
     <>
       <div className="nav-back"></div>
+
       <motion.div
         className="navbar"
         initial={{ y: -80 }}
         animate={{ y: 0 }}
         transition={{ duration: 0.45, ease: "easeIn" }}
       >
+        <div className="nav-mylogo">
+          <p>alen jo</p>
+        </div>
         <button className="nav-hamburger">
           <FontAwesomeIcon icon={faBars} />
         </button>
