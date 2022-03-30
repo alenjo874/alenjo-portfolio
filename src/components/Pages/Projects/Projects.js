b586843b5fe48a33a7b6dff729b9b279dff412ff
Alen Jo <alenjo874@gmail.com>
2022-03-30 19:16:45 -0400

    added blogger desc

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -10,7 +10,8 @@ function Projects({ scrollShowAnimation }) {
       github: "https://github.com/alenjo874/car-travel-blog-app",
       description:
         "An app for users to write blogs on cars, road trips, traveling and more.",
-      story: " ",
+      story:
+        "When building this project, I was switching back and forth from React (JavaScript) and Rails (Ruby) while also taking on learning Sass. Using Sass in this project, it helped keep my CSS code dynamic. I would love to explore more advanced ways to utilize Sass in my future projects. I used an external news api to fetch relevant articles which taught me important aspects of using different APIs. A challenge I faced during this project was setting up authentication and authorization for users. It was a great experience finally implementing cookies to persist the user information on the page.",
       image: "/images/StudiaApp.png",
       link: "https://car-travel-blog-app.herokuapp.com/blogs",
       demo: "",
